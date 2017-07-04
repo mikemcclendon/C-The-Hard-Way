b45d4b4e556e3acba2de4701ed36aa247ce8ed43
mikemcclendon <mike.mcclendon@gmail.com>
2017-07-04 19:08:16 -0400

    Third

--- ex1.c
+++ ex1.c
@@ -3,10 +3,18 @@
 /* This is a comment. */
 int main(int argc, char *argv[])
 {
-	int distance = 100;
+	int distance = 100, distance2 = 200, empty;
 	
 	// this is also a comment
 	printf("You are %d miles away. \n", distance);
+	printf("Now you are %d miles away. \n", distance2);
+	empty = distance+distance2;
+	printf("And now you are %d miles away. \n", empty);
+	empty = distance*distance2;
+	printf("Even still now %d miles away. \n", empty);
+	empty = distance2/distance;
+	printf("Now %d miles away. \n", empty);
+	
 	
 	return 0;
 }
